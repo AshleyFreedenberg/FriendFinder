6d55f5ae1893d680fd9a7bd651cf633091e340b1
Yadi <ashleyfreedenberg@gmail.com>
2019-04-01 16:16:16 -0700

    4:30pm

--- app/server.js
+++ app/server.js
@@ -9,8 +9,8 @@ app.use(express.json());
 
 //app.use(express.static('public'));
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
